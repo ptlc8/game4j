f619aebbe534e7af642354a7365091f0baa91df9
CYTech Student <CYTech Student@Student-laptop>
2022-05-16 20:39:49 +0200

    fonction recuperer  toutes les parties terminées et une pour toutes les parties en cours

--- src/fr/kmmad/game4j/Game4j.java
+++ src/fr/kmmad/game4j/Game4j.java
@@ -1,6 +1,7 @@
 package fr.kmmad.game4j;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import java.sql.*;
 import java.sql.Connection;
@@ -17,27 +18,36 @@ public class Game4j {
 	Game4j() throws SQLException{
 		//DriverManager.registerDriver(new Driver());
 		bdd = DriverManager.getConnection("jdbc:mysql://localhost:3306/game4j", Credentials.DB_USERNAME, Credentials.DB_PASSWORD);
-		System.out.println("COUCOU");
-		Statement statement = bdd.createStatement();
-		ResultSet rs = statement.executeQuery("Select * From history");
-		rs.next();
-		System.out.println(rs.getInt("id"));
 	}
 	
-	// r�cup�rer les parties finies dans l'historique 
+	// recuperer parties finies de l'historique 
 	List<Game> getHistory() throws SQLException{
 		Statement statement = bdd.createStatement();
 		ResultSet rs = statement.executeQuery("Select * From history");
-		rs.next();
-		System.out.println(rs.getInt("id"));
-		return null;
+		List<Game> recupGameFinish = new ArrayList<Game>(); 
+		for (int i=0; i <= rs.getFetchSize() ; i++) {
+			recupGameFinish.add(Game.loadSave(rs.getString("game")));
+			rs.next();
+		}
+		return recupGameFinish;
 	}
 
+	// recuperer toutes les parties en cours de la sauvegarde 
+	List<Game> getAllSaves() throws SQLException{
+		Statement statement = bdd.createStatement();
+		ResultSet rs = statement.executeQuery("Select * From saves");
+		List<Game> recupSaves = new ArrayList<Game>(); 
+		for (int i=0; i <= rs.getFetchSize() ; i++) {
+			recupSaves.add(Game.loadSave(rs.getString("game")));
+			rs.next();
+		}
+		return recupSaves;
+	}	
+	
 	// R�cup�rer une partie en cours dans la sauvegarde � partir de son nom
 	Game getSave(String nameSave) throws SQLException {
 		Statement statement = bdd.createStatement();
-		ResultSet rs = statement.executeQuery("Select * From saves");
-		rs.next();
+		ResultSet rs = statement.executeQuery("Select " +nameSave+ " From saves");
 		System.out.println(rs.getInt("id"));
 		return null;
 	}
@@ -45,7 +55,7 @@ public class Game4j {
 	// Ajouter une partie finie et la mettre dans l'historique
 	void addGameHistory(Game game) throws SQLException {
 		Statement statement = bdd.createStatement();
-		ResultSet rs = statement.executeQuery("Insert to history");
+		ResultSet rs = statement.executeQuery("Insert " +game+" to history");
 		rs.next();
 		System.out.println(rs.getInt("id"));
 	}
@@ -53,7 +63,7 @@ public class Game4j {
 	// Ajouter une partie en cours dans la sauvegarde 
 	void addGameSave(Game game) throws SQLException {
 		Statement statement = bdd.createStatement();
-		ResultSet rs = statement.executeQuery("Insert to saves");
+		ResultSet rs = statement.executeQuery("Insert "+game+ " to saves");
 		rs.next();
 		System.out.println(rs.getInt("id"));
 	}
