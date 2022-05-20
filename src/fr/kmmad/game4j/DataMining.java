91c93f4c4c805660f240af3c7015feb7dad9c3e4
Duncan <boukhssibi@cy-tech.fr>
2022-05-20 17:07:08 +0200

    Rajout du test sur console main et modification sur dataMining

--- src/fr/kmmad/game4j/DataMining.java
+++ src/fr/kmmad/game4j/DataMining.java
@@ -2,23 +2,25 @@ package fr.kmmad.game4j;
 
 public class DataMining{
 	
-	public void getQI(Game nameSave) {
+	
+	
+	public static void getQI(Game game) {
 		int qi = 0;
 		// on test si le joueur a gagné la partie 
-		if ( nameSave.isVictory()== true) {
+		if ( game.isVictory()== true) {
 			qi=qi + 5;
 		}else {
 			qi=qi+1;
 		}
 		// on test le pourcentage de bonus pris par le joueur 
-		if (nameSave.getBonusRate()/2 <= nameSave.getPlayer().getNumberBonus()) {
+		if (game.getBonusRate()/2 <= game.getPlayer().getNumberBonus()) {
 			qi=qi+3;
-		}else if(nameSave.getBonusRate()/4 <= nameSave.getPlayer().getNumberBonus()) {
+		}else if(game.getBonusRate()/4 <= game.getPlayer().getNumberBonus()) {
 			qi = qi + 1;
 		}
 		//test le pourcentage d'obstacles qui a eu dans la partie
 		// on divise par 10 le pourcentage et c'est le nombre de point que l'on donne au qi 
-		int percentageObstacles = nameSave.getPlayer().getNumberBonus()/10;
+		int percentageObstacles = game.getPlayer().getNumberBonus()/10;
 		qi = qi + percentageObstacles;
 
 		if (qi >= 8 ){
